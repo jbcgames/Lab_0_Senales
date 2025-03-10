7f78cb847d82e4559d9590f2b3f1db17eb5987e8
Jbcgames <morcitawindows@gmail.com>
2025-03-10 05:56:08 -0500

    ejercicio 2.1 listo

--- Lab_0.py
+++ Lab_0.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import pandas as pd
+import statistics as st
 def ejercicio1_1():
     arr_1d = np.linspace(3,12,4)  # (inicio, fin, numero de muestras)
     print("Arreglo usando linspace: ", arr_1d)
@@ -26,11 +27,36 @@ def ejercicio1_4(N):
     print("Matriz identidad de dimension : ", N )
     print(union)
         
+def ejercicio2_1(data):
+    df= pd.DataFrame(data)
+    Minima = 999999
+    Maxima = 0
+    Media = 0
+    i = 0
+    for nota in df['Notas']:
+        i += 1
+        if(nota > Maxima):
+            Maxima = nota
+        if(nota < Minima):
+            Minima = nota
+        Media += nota
+    Desviacion= st.stdev(df['Notas'])
+    print("Nota Minima: ",Minima)
+    print("Nota Maxima: ",Maxima)
+    print("Nota Media: ",Media/i)
+    print("Desviacion tipica: ",Desviacion)
+    
+    
+    
+#ejercicio1_1()
 
-ejercicio1_1()
+#ejercicio1_2()
 
-ejercicio1_2()
+#ejercicio1_3(3)
 
-ejercicio1_3(3)
-
-ejercicio1_4(3)
+#ejercicio1_4(3)
+data = {
+    'Nombre': ['Ana', 'Luis', 'Juan'],
+    'Notas': [4.3, 2.9, 3.7]
+}
+ejercicio2_1(data)
