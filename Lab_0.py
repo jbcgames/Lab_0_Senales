bf596394ddd08b08c8e79e4c59a91bfd11aa264c
jbcgames <morcitawindows@gmail.com>
2025-03-16 18:40:33 -0500

    Final Changes maked

--- Lab_0.py
+++ Lab_0.py
@@ -55,10 +55,34 @@ def ejercicio_3(directorio, t1, t2):
     fs, x = read(directorio)
     inicio = int(t1*fs)
     final = int(t2*fs)
-    audio_salida
-    Audio(x, rate=fs)
+    tramo = x[inicio:final]
+    return Audio(tramo, rate=fs)
 
+def ejercicio_4(lista, percentil):
+    lista.sort()
+    n = len(lista)
+    pos = (percentil / 100) * (n + 1)
     
+    # Si pos es menor que 1, el percentil es el primer elemento
+    if pos < 1:
+        return lista[0]
+    # Si pos es mayor o igual a n, el percentil es el último elemento
+    elif pos >= n:
+        return lista[-1]
+    else:
+        # Convertir pos a índice 0-basado
+        pos_int = int(pos)
+        fraccion = pos - pos_int
+        
+        # Si la posición es entera, se devuelve el valor correspondiente
+        if fraccion == 0:
+            return lista[pos_int - 1]  # restamos 1 porque la fórmula es 1-basada
+        else:
+            # Se interpola entre el valor en pos_int-1 y pos_int
+            inferior = lista[pos_int - 1]
+            superior = lista[pos_int]
+            return inferior + fraccion * (superior - inferior)
+
 #ejercicio1_1()
 
 #ejercicio1_2()
@@ -76,4 +100,7 @@ def ejercicio_3(directorio, t1, t2):
 
 #print(ejercicio2_2("dataframe_lab0.xlsx"))
 
-ejercicio_3("audio.wav",2, 3)
+#ejercicio_3("audio.wav",2, 3)
+
+Lista = [5, 2, 3, 7, 1, 10]
+print(ejercicio_4(Lista, 50))
